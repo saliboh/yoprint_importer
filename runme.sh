35d3189ccf5199635802293601b4864fb55aef38
Andrean Earl <earl@nabepero.co.jp>
2022-05-07 11:50:21 +0800

    build: fixed horizon issue with local env

--- runme.sh
+++ runme.sh
@@ -1,5 +1,7 @@
 cp src/.env.example src/.env
-docker-compose run --rm composer install
+docker-compose run --rm composer install --ignore-platform-reqs
 docker-compose run --rm artisan key:generate
 docker-compose run --rm artisan migrate:fresh
 docker-compose run --rm artisan storage:link
+docker-compose run --rm artisan horizon:install
+docker-compose run --rm artisan horizon
